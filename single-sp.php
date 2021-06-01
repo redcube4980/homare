a6e0f770f2198ad7e1cafee5b124d4b72bb84265
yamaki <yamaki@yamakiMacBook-Pro.local>
2021-06-01 16:28:03 +0900

    2021/06/01/16:27

--- single-sp.php
+++ single-sp.php
@@ -14,17 +14,17 @@ Template Name: sp
 		remove_filter( 'the_content', 'wpautop' );
 		remove_filter( 'the_excerpt', 'wpautop' );
 		the_content();
+
+	// Previous/next post navigation.
+	the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">次へ</span><i class="fa fa-angle-right"></i>',
+				'prev_text' => '<i class="fa fa-angle-left"></i><span class="meta-nav" aria-hidden="true">前へ</span> ',
+				'screen_reader_text' => '前後の記事へ',
+			) );
+
 	endwhile;
 endif;
 ?>
-	<nav class="navigation post-navigation" role="navigation">
-		<h2 class="screen-reader-text">前後の記事へ</h2>
-		<div class="nav-links">
-			<div class="nav-previous">
-				
-			</div>
-		</div>
-	</nav>
 </div>
 </main><!-- .site-main -->
 
